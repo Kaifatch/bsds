8dc5f3b97bec20984c9aa8e6443853fa9ad9599f
Kaifatch <suvorov63ru@ya.ru>
2020-08-27 20:59:43 +0400

    Updates

---
+++ precache-manifest.fd7c41c4ffd735ae1b30c38e8435948e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64bc690d242500ac809aab4a0362e1cc",
+    "url": "/bsds/index.html"
+  },
+  {
+    "revision": "2ae61af272f9955894f8",
+    "url": "/bsds/static/css/main.36c4608a.chunk.css"
+  },
+  {
+    "revision": "b28d615637e1d8e28eac",
+    "url": "/bsds/static/js/2.00664321.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bsds/static/js/2.00664321.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ae61af272f9955894f8",
+    "url": "/bsds/static/js/main.6a8604f0.chunk.js"
+  },
+  {
+    "revision": "565f2cd0ef9f488b942d",
+    "url": "/bsds/static/js/runtime-main.aec7066e.js"
+  },
+  {
+    "revision": "28237aca4598c74d1c4259653db03915",
+    "url": "/bsds/static/media/background.28237aca.mp4"
+  },
+  {
+    "revision": "cc549709ef173b1da34acce5aff7c59d",
+    "url": "/bsds/static/media/logo.cc549709.png"
+  }
+]);
